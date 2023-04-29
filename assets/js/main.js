98605615cdca176cd74ebac5f62099b34e858e49
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-29 18:05:36 +1000

    popular

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,21 @@
 /*  CHANGE BACKGROUND HEADER  */
 function scrollHeader() {
-    const header = document.getElementById("header");
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 50) header.classList.add("scroll-header");
-    else header.classList.remove("scroll-header");
-  }
-  window.addEventListener("scroll", scrollHeader);
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* SWIPER POPULAR */
+let swiperPopular = new Swiper(".popular_container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
